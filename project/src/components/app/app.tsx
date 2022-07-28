ab251de5f40f027067c183ef4e8e025538f138e0
maxim1436 <pivanov_m@mail.ru>
2022-07-28 13:13:09 +1000

    разбил на компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPageScreen from '../../pages/main-page-screen/main-page-screen';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return <MainPageScreen offersCount = {offersCount}/>;
 }
 
 export default App;
